03ecefe7898289348e3b1117153e491cb4cab741
VortexRK <vortex.rk@gmail.com>
2021-07-31 16:43:23 +0300

    add full site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import Home from './components/Home'
+import Blog from './components/Blog';
+import Footer from './components/Footer'
+import Works from './components/Works';
+import NoMatch from './components/NoMatch';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/works'>
+          <Works />
+        </Route>
+        <Route path='/blog'>
+          <Blog />
+        </Route>
+        <Route component={NoMatch} />
+      </Switch>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Works.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components/macro'
+import { Switch, Route, useRouteMatch } from 'react-router-dom'
+import WorksList from './Works/WorksList'
+import DesigningDashboard from './Works/DesigningDashboard'
+import portrait from './images/portrait.jpg'
+import dashboard from './images/dashboard.jpg'
+import malayalam from './images/malayalam.jpg'
+import components from './images/components.jpg'
+
+const Works = props => {
+  const works = [
+    { id: 1, header: 'Designing Dashboards. CLICK ME', image: dashboard, year: 2020, type: 'Dashboard', path: '/design', text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.' },
+    { id: 2, header: 'Vibrant Portraits of 2020', image: portrait, year: 2018, type: 'Illustration', path: '', text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.' },    
+    { id: 3, header: '36 Days of Malayalam type', image: malayalam, year: 2019, type: 'Typography', path: '', text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.' },
+    { id: 4, header: 'Components', image: components, year: 2018, type: 'Components, Design', path: '', text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.' },
+  ]
+
+  const { path } = useRouteMatch()
+
+  return (
+    <main className={props.className}>
+      <WorksWrapper>
+        <Switch>
+          <Route exact path={path}>
+            <Header> Work </Header>
+            {works.map(work => <WorksList work={work} key={work.id} />)}
+          </Route>
+          <Route path={`${path}/design`}>
+            <DesigningDashboard />
+          </Route>
+        </Switch>
+      </WorksWrapper>
+    </main>
+  )
+}
+
+Works.propTypes = {
+  className: PropTypes.string,
+}
+
+const WorksWrapper = styled.section`
+  width: 682px;
+  padding: 25px 0px 25px;
+
+  @media (max-width: 720px) {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    padding:0 20px 25px;
+  }
+`
+
+const Header = styled.h1`
+  font-size: 44px;
+  font-weight: 900;
+  line-height: 60px;
+  margin: 0 0 30px;
+
+  @media (max-width: 720px) {
+    font-size: 35px;
+  }
+`
+
+export default styled(Works)`
+  display: flex;
+  justify-content: center;
+  flex: 1 0 auto;
+  width: 100%;
+`
